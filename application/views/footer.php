a072b54d62b1f64df6c002f137ffd6fdd0ecc4a0
Langi Yudhistiara <yudis.net42@gmail.com>
2014-09-07 19:57:39 +0700

    first commit
    first commit

---
+++ application/views/footer.php
@@ -0,0 +1,7 @@
+<hr>
+<div class="container">
+  <small><a herf="/" style="color:grey">&#64;Pasteit</a></small>
+</div>
+
+</body>
+</html>
---
+++ application/views/welcome_message.php
@@ -0,0 +1,35 @@
+<div id="name" class="container">
+
+  <br>
+  <br>
+
+  <h1>
+    <center>Pasteit, <br>copy and paste your content to get your catalogue
+    <br>
+    <br>
+    <small style="color:grey">based on DDC</small>
+    </center>
+  </h1>
+
+  <hr>
+
+
+  <table border="0">
+    <tr>
+      <td style="width:33%"></td>
+      <td style="width:33%">
+      
+    <form action="/" method="POST" accept-charset="utf-8">
+      
+      <textarea name="content" style="width:600px;" placeholder="your content here"></textarea>
+      <br>
+      <button class="button button-primary" style="float:right">Analyze it</button>
+
+    </form>
+      </td>
+      <td style="width:33%"></td>
+    </tr>
+  </table>
+
+
+</div>
