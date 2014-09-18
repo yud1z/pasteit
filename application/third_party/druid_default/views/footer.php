b30dead0b10e24913ed235992f5d384f2b75601c
Langi Yudhistiara <yudis.net42@gmail.com>
2014-09-18 21:28:43 +0700

    Foundation
    Signed-off-by: Langi Yudhistiara <yudis.net42@gmail.com>

--- application/third_party/druid_default/views/footer.php
+++ application/third_party/druid_default/views/footer.php
@@ -1,4 +1,3 @@
-  <div class="clear"></div>
-</div>
-</body>
+  </body>
+
 </html>
--- application/third_party/druid_default/views/header.php
+++ application/third_party/druid_default/views/header.php
@@ -1,39 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>PT PARAHITA PRIMA SENTOSA</title>
-<link href="/{path}files/css/screen.css" rel="stylesheet">
-<link href="/{path}files/css/style.css" rel="stylesheet">
-<link rel="stylesheet" href="/{path}files/css/themes/default/dark.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="/{path}files/css/nivo-slider.css" type="text/css" media="screen" />
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  
+    <title>Pasteit</title>
+    
 
-<script src="/{path}/files/js/jquery.min.js"></script>
-<script type="text/javascript" src="/{path}/files/js/jquery.nivo.slider.js"></script>
+  </head>
 
-<script type="text/javascript">
-$(window).load(function() {
-    $('#slider').nivoSlider();
-    });
-</script>
-</head>
-
-<body>
-</body>
-
-<div id="wrapper">
-  <header>
-    <img src="/{path}/files/img/haeder.jpg" width="980">
-  </header>
-
-  <div id="nav">
-    <ul>
-      <li><a href="/">HOME</a></li>
-      <li><a href="/profile">PROFILE</a></li>
-      <li><a href="/products">PRODUCTS</a></li>
-      <li><a href="/clients">CLIENTS</a></li>
-      <li><a href="/contact_us">CONTACT US</a></li>
-      <li><a>&nbsp;</a></li>
-
-    </ul>
-  </div>
-
+  <body>
